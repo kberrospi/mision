65d99e989ca2a1d011e0fb3f039ca8ee1e78f08c
Goye <karlos.goye@gmail.com>
2020-05-05 20:30:17 -0500

    fix: issue with asynchronicity

--- app.js
+++ app.js
@@ -16,30 +16,28 @@ mongoose.connection.on("error", function(e) {
 app.use( express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(express.static("public"));
-
-
 
 app.listen(3000, ()=>{
     return console.log("Listening on port 3000");
 });
 
-app.get("/",  (req, res)=>{
-
+app.get("/users",  async(req, res)=>{
+  const users = await User.find({});
+  res.send(JSON.stringify(users));
 });
 
-app.post("/", async function(req, res){
-    let usr = req.body.userName;
-    let email = req.body.correo;
-    let pwd = req.body.pass; 
-    const salt = bcrypt.genSaltSync(1);
-    let crypt = bcrypt.hashSync(pwd,salt);
-    
+app.post("/register", async function(req, res){
+  let usr = req.body.userName;
+  let email = req.body.correo;
+  let pwd = req.body.pass; 
+  const salt = bcrypt.genSaltSync(1);
+  let crypt = bcrypt.hashSync(pwd,salt);
+  
+  try {
     await User.create({userName: usr, correo: email, pass: crypt });
-
-    await User.find({}, (err, user)=>{
-        console.log(JSON.stringify(user));
-        return res.send(JSON.stringify(user));
-       
-    }); 
+    return res.sendStatus(200);
+  } catch(e) {
+    return res.status(500).send('Se presento un error' + e.message);
+  }
+  
 });
--- public/index.js
+++ public/index.js
@@ -6,24 +6,29 @@ $(document).ready(function() {
     let name=""; let email=""; let passw ="";
     name = $('#user').val(); email = $('#correo').val(); passw = $('#passcode').val();
 
+    console.log('name', name);
     $.ajax({
       method: "POST",
-      url: "/",
+      url: "/register",
       data: JSON.stringify({ userName: name, correo: email, pass: passw }),
-      contentType: "application/json; charset=utf-8", //"application/json"
-      dataType: "json",
-    }).done(function(data){
-      let html = "";
-      data.forEach(element => {
-        html += `<tr><td>${element.userName}</th><td>${element.correo}</th><td>${element.pass}</th></td>`;
+      contentType: "application/json", //"application/json"
+    }).done(function(){
+      $('form').hide();
+      $('h1').replaceWith('<h1 class="text-center">Usuarios registrados</h1>')
+      $('#tabla').append('<table class="table table-bordered text-center" id="usuarios"><thead><tr><th>Nombre</th><th>Email</th><th>Contraseña</th></tr></thead><tbody>');
+      $.ajax({
+        method: "GET",
+        url: "/users",
+        contentType: "application/json",
+        dataType: "json",
+      }).done(function(data){
+        let html = "";
+        data.forEach(element => {
+          html += `<tr><td>${element.userName}</th><td>${element.correo}</th><td>${element.pass}</th></td>`;
+        });
+        $('#usuarios').append(html + "</tbody></table>");
+        $('.wrapper').append('<a href="http://localhost:3000/">Volver </a>');
       });
-      $('#usuarios').append(html + "</tbody></table>");
-      $('.wrapper').append('<a href="http://localhost:3000/">Volver </a>');
     });
-
-    $('form').hide();
-    $('h1').replaceWith('<h1 class="text-center">Usuarios registrados</h1>')
-    $('#tabla').append('<table class="table table-bordered text-center" id="usuarios"><thead><tr><th>Nombre</th><th>Email</th><th>Contraseña</th></tr></thead><tbody>');
-
   });
 });
